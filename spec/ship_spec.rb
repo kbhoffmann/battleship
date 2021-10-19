2948d7d3fe5901076f1bb62ab13bd8184178bdba
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-10-18 19:20:41 -0500

    Test: add test for attributes, name and length

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Ship do
 
     expect(cruiser).to be_an_instance_of(Ship)
   end
+
+  it "has attributes" do
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser.name).to eq("Cruiser")
+    expect(cruiser.length).to eq(3)
+  end
 end
