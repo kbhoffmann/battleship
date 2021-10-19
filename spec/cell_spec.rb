d2fb75c5dc73c37c24c3f9a7c7850f034aa3714f
David Kassin <david.j.kassin@gmail.com>
2021-10-19 07:40:07 -0600

    Add ship method test

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -6,4 +6,14 @@
      cell = Cell.new("B4")
      expect(cell).to be_a Cell
    end
+
+  it 'returns cell coordinate' do
+    cell = Cell.new("B4")
+    expect(cell.coordinate).to eq ("B4")
+  end
+
+  it '#ship returns nil when empty' do
+    cell = Cell.new("B4")
+    expect(cell.ship).to eq (nil)
+  end
 end
