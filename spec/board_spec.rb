184bb67b0bc3944078f6d6d847d6a984b0d2fea5
David Kassin <david.j.kassin@gmail.com>
2021-10-21 08:32:15 -0600

    test: Validate if ship placement is true for consecutive coordinates

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -29,12 +29,24 @@ RSpec.describe Board do
     expect(board.valid_coordinate?("A22")).to be(false)
   end
 
-  it 'Validates if a ship placement is true' do
+  xit 'Validates if a ship placement is true for ship length' do
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
     expect(board.valid_placement?(cruiser, ["A1","A2"])).to eq(false)
     expect(board.valid_placement?(submarine, ["A2","A3","A4"])).to eq(false)
   end
+
+  it 'Validates if a ship placement is true for consecutive coordinates' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    expect(board.valid_placement?(cruiser, ["A1","A2","A4"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A1","C1"])).to eq(false)
+    expect(board.valid_placement?(cruiser, ["A3","A2","A1"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["C1","B1"])).to eq(false)
+  end
+
+
 
 end
