a779c5979a5fab82da85f698775450b4ea714ef9
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-10-20 20:46:55 -0500

    Test: add test for creating dictionary of cells

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,11 +1,21 @@
 require './lib/board'
 require './lib/cell'
 require './lib/ship'
+require 'pry'
 
 RSpec.describe Board do
   it 'exists' do
     board = Board.new
 
     expect(board).to be_an_instance_of(Board)
+  end
+
+  #Should we test for @coordinates
+
+  it 'keeps a dictionary of cells' do
+    board = Board.new
+
+    expect(board.cells.length).to eq(16)
+    expect(board.cells).to be_a(Hash)
   end
 end
