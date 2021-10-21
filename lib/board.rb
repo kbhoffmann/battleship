f80b8e8bd6e0bd1b6e4ebe7e8b9ba4716d031ff2
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-10-20 20:47:53 -0500

    Feat: add 'cells' method to create cells has; tests pass

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,16 @@
 class Board
-end 
+  def initialize
+    @coordinates = ["A1", "A2", "A3", "A4",
+                    "B1", "B2", "B3", "B4",
+                    "C1", "C2", "C3", "C4",
+                    "D1", "D2", "D3", "D4"]
+  end
+
+  def cells
+    cells = Hash.new
+    @coordinates.each do |coordinate|
+     cells[coordinate] = Cell.new(coordinate)
+    end
+    cells
+  end
+end
