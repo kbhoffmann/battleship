04a3554120a8233730c9e79b44d31fc5e932c05b
David Kassin <david.j.kassin@gmail.com>
2021-10-21 14:28:46 -0600

    feat: consecutive_numbers method

--- lib/board.rb
+++ lib/board.rb
@@ -18,6 +18,13 @@ class Board
     @coordinates.include?(coordinate)
   end
 
+  def consecutive_numbers(array)
+    if array.length == 3
+      cruiser_check = array.each_cons(3).any?{ |x,y| x == y - 1}
+    else
+      sub_check = array.each_cons(2).any?{ |x,y| x == y - 1}
+    end
+  end
 
   def valid_placement?(ship_type, coordinates)
     # This long group of .each and .map breaks up the coordinates into two arrays
@@ -32,40 +39,37 @@ class Board
     coordinates.each do |coordinate|
       split_array << coordinate.split("")
     end
+    split_array
     # This seperates the two strings into two arrays. One array with letters and one array with numbers
-    split_array.map do |letter|
+    split_array.each do |letter|
       letter_array << letter.first
       number_array << letter.last.to_i
     end
-
+    letter_array
+    number_array
     # This creates a seperate array that converts the letters array into numbers to determine consecutive letters
-    letter_array.map do |letter|
+    letter_array.each do |letter|
       letter_array_ord << letter.ord
     end
+    letter_array
+
+    # number_consecutive = number_array.include?()
+
+
 
     ## end of potential helper method
     ## These are tests that can return boolean values to test placement
 
-    # If the difference between the max and min is greater then -3 for Cruiser
-    # and -2 for subs then the numbers are consecutive
-    # I think we can write a better method for checking consecutive
-
-    consecutive_check_num = number_array.min - number_array.max
-    consecutive_check_let = letter_array_ord.min - letter_array_ord.max
     # This returns a boolean if the letters or numbers are all the letter_same
     letter_same = letter_array.all?
     number_same = number_array.all?
-    # This checks the numbers
-    consecutive_cruis_num = consecutive_check_num > -3
-    consecutive_cruis_let = consecutive_check_let > -3
-    consecutive_sub_num = consecutive_check_num > -2
-    consecutive_sub_let = consecutive_check_let > -2
+
     ## My thoughts were if we could create tests that return booleans
     ## we could make a very simple if statement that just had all the tests
-    test1 = letter_same && number_same == false && consecutive_cruis
-    test2 = letter_same == false && number_same && consecutive_sub_let
-
-    if ship_type.length == coordinates.count && test1 && test2
-    end
+    # test1 = letter_same
+    # test2 = letter_same == false
+    # test3 = number_same
+    # test4 = number_same == false
+    # ship_legnth = ship_type.length == coordinates.count
   end
 end
