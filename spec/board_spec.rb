7cc4150fda836d7e4d659f4d14bd601701cc7501
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-10-21 20:01:10 -0500

    Test: add test for placing ships in cells

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -90,4 +90,18 @@ RSpec.describe Board do
     expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
   end
 
+  xit 'can place ships in its cells ' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    cell_1 = board.cells["A1"]
+    cell_2 = board.cells["A2"]
+    cell_3 = board.cells["A3"]
+
+    cell_1.ship
+    cell_2.ship
+    cell_3.ship
+
+    expect(cell_3.ship == cell_2.ship).to eq(true)
+  end
 end
