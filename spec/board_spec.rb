38b5efa951b50274e07a3b5c463684f0596b3089
David Kassin <david.j.kassin@gmail.com>
2021-10-22 08:44:55 -0600

    test: Add board.cells method to test that places ships in cells

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -93,6 +93,7 @@ RSpec.describe Board do
   it 'can place ships in its cells ' do
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
+    board.cells
     board.place(cruiser, ["A1", "A2", "A3"])
     cell_1 = board.cells["A1"]
     cell_2 = board.cells["A2"]
