2cc474ca36b1780f328824076e53072dee800d01
David Kassin <david.j.kassin@gmail.com>
2021-10-23 10:26:33 -0600

    feat: Add starter method to Game class

--- lib/game.rb
+++ lib/game.rb
@@ -5,4 +5,19 @@ require './lib/ship'
 class Game
   def initialize
   end
+
+  def starter(answer)
+    if answer == "p"
+      p "Game will start"
+    elsif answer == "q"
+      p "Good Bye"
+      exit!
+    else
+      p "Invalid Response"
+      puts "Enter p to play. Enter q to quit."
+      new_answer = gets.chomp
+      self.starter(new_answer)
+    end
+  end
+
 end
