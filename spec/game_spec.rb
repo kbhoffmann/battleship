819cbfb0b2d43ad366d82440ce695684b560139d
David Kassin <david.j.kassin@gmail.com>
2021-10-23 10:25:59 -0600

    test: Show q and p responses work

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,4 +9,15 @@ describe Game do
 
     expect(game).to be_a Game
   end
+
+  it 'takes user input and starts game' do
+    game = Game.new
+    answer = "p"
+
+    expect(game.starter(answer)).to eq("Game will start")
+
+    answer = "q"
+    expect(game.starter(answer)).to eq("Good Bye")
+
+  end
 end
