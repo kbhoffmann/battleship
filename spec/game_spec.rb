1a613d86681a0976a1db09f16fc983d56a0e3017
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-10-24 08:58:16 -0500

    Test: add test for initializing game with player ships

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -17,4 +17,12 @@ describe Game do
 
     expect(game.player_board).to be_an_instance_of(Board)
   end
+
+  it 'starts with a player ships' do
+    game = Game.new
+
+    expect(game.player_cruiser).to be_an_instance_of(Ship)
+    expect(game.player_sub).to be_an_instance_of(Ship)
+  end
+
 end
