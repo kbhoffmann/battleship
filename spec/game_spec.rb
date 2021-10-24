d44af58f4f8c003b297e8a26cc2f2b425acfcde5
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-10-24 08:57:01 -0500

    Test: add test for initializing game with player board

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -12,4 +12,9 @@ describe Game do
     expect(game).to be_a Game
   end
 
+  it 'starts with a player board' do
+    game = Game.new
+
+    expect(game.player_board).to be_an_instance_of(Board)
+  end
 end
