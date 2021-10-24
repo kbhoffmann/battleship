e2bc5a349771824d8e19548501e6655a6b4ffb06
David Kassin <david.j.kassin@gmail.com>
2021-10-24 17:41:16 -0600

    feat: Edit player_cruiser_coords_formated method

--- lib/game.rb
+++ lib/game.rb
@@ -12,13 +12,15 @@ class Game
     @player_board = Board.new
     @player_cruiser = Ship.new("Cruiser", 3)
     @player_sub = Ship.new("Submarine", 2)
+    @player_cruiser_input = nil
+    @player_sub_input = nil
   end
 
   def starter(answer)
     if answer == "p"
       p "Game will start"
       #Computer places ships on computer board, then player is prompt to place cruiser
-      player_cruiser_placement_instructions
+      # player_cruiser_placement_instructions
     elsif answer == "q"
       p "Good Bye"
       exit!
@@ -30,33 +32,34 @@ class Game
     end
 
   end
-  def player_cruiser_placement_instructions
-      puts "I have laid out my ships on the grid.
-      You now need to lay out your two ships.
-      The Cruiser is three units long and the Submarine is two units long.
-        1 2 3 4
-      A . . . .
-      B . . . .
-      C . . . .
-      D . . . .
-      Enter the 3 sets of coordinates for the Cruiser (example: A4 B4 C4 ):"
-      #need them separated by comma? no commas? no spaces? change our code to be case insensitive?
-      cruiser_player_input = gets.chomp
-  end
+  # def player_cruiser_placement_instructions
+  #     puts "I have laid out my ships on the grid.
+  #     You now need to lay out your two ships.
+  #     The Cruiser is three units long and the Submarine is two units long.
+  #       1 2 3 4
+  #     A . . . .
+  #     B . . . .
+  #     C . . . .
+  #     D . . . .
+  #     Enter the 3 sets of coordinates for the Cruiser (example: A4 B4 C4 ):"
+  #     #need them separated by comma? no commas? no spaces? change our code to be case insensitive?
+  #     cruiser_player_input = gets.chomp
+  # end
 
   def player_cruiser_coords_formatted
     #mocking the gets.chomp, will have to remove this variable later
-    cruiser_player_input = "A4 B4 C4"
-    binding.pry
-    player_cruiser_placement_instructions.split
+    cruiser_player_input = gets.chomp
+    # cruiser_player_input = "A4 B4 C4"
+    @player_cruiser_input = cruiser_player_input.split
       #=> ["A1", "B1", "C1"]
+    @player_cruiser_input
   end
 
 
   #------------------------------------------------------
   #This is where I left off for now.  Ran into issues, may need to rethink some things(sorry I know thats vague)
     def check_player_cruiser_placement_coords(coordinates)
-      binding.pry
+
       #?MAYBE MAKE VALID PLACEMENT A MODULE INSTEAD OF LEAVING IT IN BOARD CLASS??? or recreate valid placement in game
       # coordinates = player_cruiser_coords_formatted
       if @player_board.valid_placement?(@player_cruiser, coordinates)
@@ -95,38 +98,3 @@ class Game
     end
 
 end
-
-
-#   def place_player_cruiser
-#     ship = @cruiser
-#     coord = board.coordinates
-#     all_combos = coord.combination(3).to_a
-#     all_combos.each do |combo|
-#       if board.valid_placement?(ship, combo)
-#         @valid_combo_cruiser << combo
-#     #elsif board.valid_placement? == false
-#       # p Those are invalid coordinates. Please try again:
-#       #gets.chomp
-#     #make user input loop, exit if valid_placement? == true
-#       end
-#     end
-#     @valid_combo_cruiser
-#   #do we still have to shuffle this when the player is choosing locations?
-#     cruiser_placement = valid_combo_cruiser.shuffle.first
-#     board.place(cruiser, cruiser_placement)
-#     board.render(true)
-#   end
-#
-#   def place_player_sub
-#     all_combos = board.coordinates.combination(2).to_a
-#     all_combos.each do |combo|
-#       if board.valid_placement?(submarine, combo)
-#         @valid_combo_sub << combo
-#       end
-#     end
-#     @valid_combo_sub
-# #do we still have to shuffle this when the player is choosing locations?
-#     sub_placement = valid_combo_sub.shuffle.first
-#     board.place(submarine, sub_placement)
-#     board.render(true)
-#   end
