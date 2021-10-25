5ef99b6a0d0b0d60053e7da21299404d93b57f36
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-10-25 15:24:21 -0500

    Feat: add display_boards method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -24,7 +24,7 @@ describe Game do
     expect(game.player_cruiser).to be_an_instance_of(Ship)
     expect(game.player_sub).to be_an_instance_of(Ship)
   end
-  
+
   it 'formats player input for cruiser' do
     game = Game.new
 
@@ -42,47 +42,52 @@ describe Game do
     expect(game.check_player_cruiser_placement_coords(["B1", "B3", "C1"])).to eq(false)
   end
 
-  xit 'can let the player place a cruiser ship' do
-    game = Game.new
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-    board.cells
-    board.place(cruiser, ["B2", "B3", "B4"])
+    xit '' do
 
-    expected_1 = "  1 2 3 4 \nA . . . . \nB . S S S \nC . . . . \nD . . . . \n"
-
-    expected_2 = "  1 2 3 4 \n" +
-                 "A . . . . \n" +
-                 "B . S S S \n" +
-                 "C . . . . \n" +
-                 "D . . . . \n"
-
-    expect(game.place_player_cruiser(cruiser).length).to eq(3)
-    # expect(game.board.render(true)).to eq(expected_1)
-    # expect(game.board.render(true)).to eq(expected_2)
-  end
-
-  xit 'can let the player place a submarine' do
-    game = Game.new
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-    board.cells
-
-    board.place(cruiser, ["B2", "B3", "B4"])
-    board.place(submarine, ["C1", "D1"] )
-
-    expected_1 = "  1 2 3 4 \nA . . . . \nB . S S S \nC S . . . \nD S . . . \n"
-
-    expected_2 = "  1 2 3 4 \n" +
-                 "A . . . . \n" +
-                 "B . S S S \n" +
-                 "C S . . . \n" +
-                 "D S . . . \n"
-
-    expect(game.sub_placement.length).to eq(3)
-    expect(game.board.render(true)).to eq(expected_1)
-    expect(game.board.render(true)).to eq(expected_2)
-  end
+    expect(game.display_boards).to eq(expected)
 end
+
+#   xit 'can let the player place a cruiser ship' do
+#     game = Game.new
+#     board = Board.new
+#     cruiser = Ship.new("Cruiser", 3)
+#     submarine = Ship.new("Submarine", 2)
+#     board.cells
+#     board.place(cruiser, ["B2", "B3", "B4"])
+#
+#     expected_1 = "  1 2 3 4 \nA . . . . \nB . S S S \nC . . . . \nD . . . . \n"
+#
+#     expected_2 = "  1 2 3 4 \n" +
+#                  "A . . . . \n" +
+#                  "B . S S S \n" +
+#                  "C . . . . \n" +
+#                  "D . . . . \n"
+#
+#     expect(game.place_player_cruiser(cruiser).length).to eq(3)
+#     # expect(game.board.render(true)).to eq(expected_1)
+#     # expect(game.board.render(true)).to eq(expected_2)
+#   end
+#
+#   xit 'can let the player place a submarine' do
+#     game = Game.new
+#     board = Board.new
+#     cruiser = Ship.new("Cruiser", 3)
+#     submarine = Ship.new("Submarine", 2)
+#     board.cells
+#
+#     board.place(cruiser, ["B2", "B3", "B4"])
+#     board.place(submarine, ["C1", "D1"] )
+#
+#     expected_1 = "  1 2 3 4 \nA . . . . \nB . S S S \nC S . . . \nD S . . . \n"
+#
+#     expected_2 = "  1 2 3 4 \n" +
+#                  "A . . . . \n" +
+#                  "B . S S S \n" +
+#                  "C S . . . \n" +
+#                  "D S . . . \n"
+#
+#     expect(game.sub_placement.length).to eq(3)
+#     expect(game.board.render(true)).to eq(expected_1)
+#     expect(game.board.render(true)).to eq(expected_2)
+#   end
+# end
