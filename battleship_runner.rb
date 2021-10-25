f63446e47116b3759dda9889daa8f81a5d9a4a3e
David Kassin <david.j.kassin@gmail.com>
2021-10-25 17:47:29 -0600

    Refactor runner to only have game.starter

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -1,42 +1,6 @@
-require './lib/board'
-require './lib/cell'
-require './lib/ship'
+
 require './lib/game'
 
-board_comp = Board.new
-board_player = Board.new
-cruiser_comp = Ship.new("Cruiser", 3)
-cruiser_player = Ship.new("Cruiser", 3)
-submarine_comp = Ship.new("Submarine", 2)
-submarine_player = Ship.new("Submarine", 2)
-board_comp.cells
-board_player.cells
 game = Game.new
 
-puts "Welcome to BATTLESHIP"
-puts "Enter p to play. Enter q to quit."
-answer = gets.chomp
-game.starter(answer)
-
-# This does computer ship placement
-board_comp.cruiser_placement(cruiser_comp)
-board_comp.sub_placement(submarine_comp)
-## This isn't necessary but I put it here to make sure it works
-p board_comp.cruiser_loc
-p board_comp.sub_loc
-
-
-puts "I have laid out my ships on the grid."
-puts "You now need to lay out your two ships."
-puts "The Cruiser is three units long and the Submarine is two units long."
-puts board_player.render(true)
-puts "Enter the squares for the Cruiser (3 spaces):"
-# game.player_cruiser_coords_formatted
-board_player.player_ship_placement(cruiser_player, game.player_coords_formatted)
-puts board_player.render(true)
-puts "Enter the squares for the Submarine (2 spaces):"
-board_player.player_ship_placement(submarine_player, game.player_coords_formatted)
-puts board_player.render(true)
-puts board_comp.render(true) # This is just to make sure we are ready to run turn.
-
-game.display_boards(board_comp.render, board_player.render(true))
+game.starter
